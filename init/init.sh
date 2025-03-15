4e16add6e8afdd7ebb0c0b7347aa881a62c8b691
Rui Lopes <rgl@ruilopes.com>
2025-03-15 11:39:27 +0000

    use certificates from private ca

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
+# update the trusted ca certificates.
+# NB this is required to trust the wso2am ca.
+# NB the wso2am ca is volume mounted inside the
+#    /usr/local/share/ca-certificates directory.
+update-ca-certificates
+
 # initilize wso2am.
 install -d tmp
 python3 \
